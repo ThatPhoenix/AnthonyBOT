6f65083c2326b82e8ffb2d788ed1e069059c2489
coolpixire <theemaraldblock@gmail.com>
2018-08-13 19:50:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const Discord = require("discord.js");
 const Anthony = new Discord.Client();
 
 Anthony.on('ready', () => {
- Anthony.user.setActivity(`${bot.guilds.size} Servers! | !invite`,{type: 'WATCHING'});
+ Anthony.user.setActivity(`${Anthony.guilds.size} Servers! | !invite`,{type: 'WATCHING'});
 });
+
+
 Anthony.login(process.env.TOKEN)
