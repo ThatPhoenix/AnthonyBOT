68e7d833059977ea42628798837bd7e88680a9b6
coolpixire <theemaraldblock@gmail.com>
2018-08-13 20:18:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const Anthony = new Discord.Client();
-const AnthonyCONFIG = ("./Config.json")
+const Config = ("./config.json")
 
 Anthony.on('ready', () => {
  Anthony.user.setActivity(`${Anthony.guilds.size} Servers! | !invite`,{type: 'WATCHING'});
@@ -9,7 +9,7 @@ console.log("[PROCESS] [ANTHONY] Anthony online!")
 
 Anthony.on("message", async message => {
  if (message.content.startsWith(AnthonyCONFIG.prefix + "invite")) {
-  message.channel.send(AnthonyCONFIG.invite)
+  message.channel.send("https://discordapp.com/api/oauth2/authorize?client_id=465539661582958592&permissions=8&scope=bot")
  }
 });
 
