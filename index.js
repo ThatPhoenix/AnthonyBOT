2e11fa3f5e0001ca2219f13c2859e3c88aaba71a
coolpixire <theemaraldblock@gmail.com>
2018-08-13 20:31:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const Discord = require("discord.js");
 const Anthony = new Discord.Client();
-const Config = ("./config.json")
+const config = require("./config.json");
 
 Anthony.on('ready', () => {
  Anthony.user.setActivity(`${Anthony.guilds.size} Servers! | !invite`,{type: 'WATCHING'});
 console.log("[PROCESS] [ANTHONY] Anthony online!")
 });
 
+
 Anthony.on("message", async message => {
- if (message.content.startsWith(Config.prefix + "invite")) {
+  // This event will run on every single message received, from any channel or DM.
+  
+  // It's good practice to ignore other bots. This also makes your bot ignore itself
+  // and not get into a spam loop (we call that "botception").
+  if(message.author.bot) return;
+  
+  // Also good practice to ignore any message that does not start with our prefix, 
+  // which is set in the configuration file.
+  if(message.content.indexOf(config.prefix) !== 0) return;
+  
+  // Here we separate our "command" name, and our "arguments" for the command. 
+  // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
+  // command = say
+  // args = ["Is", "this", "the", "real", "life?"]
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+ 
+ if (message.content.startsWith(config.prefix + "invite")) {
   message.channel.send("https://discordapp.com/api/oauth2/authorize?client_id=465539661582958592&permissions=8&scope=bot")
  }
 });
