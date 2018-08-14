a94b0e7c41b434d53269bf0f087dc30185cbabef
coolpixire <theemaraldblock@gmail.com>
2018-08-14 18:14:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 const Anthony = new Discord.Client();
 const fs = require("fs");
 const config = require("./config.json");
+const AnthonyError = require("../utils/errors.js")
 
 Anthony.on('ready', () => {
  Anthony.user.setActivity(`${Anthony.guilds.size} Servers! | !invite`,{type: 'WATCHING'});
@@ -26,7 +27,82 @@ if(command === "invite") {
   if(command === "ping") {
     const PingMSG = await message.channel.send("Pinging Around The World :D");
     PingMSG.edit(`Pong! Anthony's Ping is ${PingMSG.createdTimestamp - message.createdTimestamp}ms. API's ping is ${Math.round(Anthony.ping)}ms`);
- }
+
+  Anthony.on('message', message => {
+	if (message.author.id === config.OwnerID) {
+		let msg = message.content;
+
+		if (msg.startsWith("!eval")) {
+   		msg = msg.substring("!eval ".length)
+   		msg = msg.replace(/```js/gi, "")
+   		msg = msg.replace(/```/gi, "")
+
+   		try {
+   			let result = eval(msg)
+
+    		if (typeof(result) == "undefined") {
+    			message.channel.send({
+    				embed: {
+    					description: "This code didn't return any value",
+    					fields: [
+    						{
+    							name: ":inbox_tray: Input",
+    							value: "```" + msg + "```"
+    						},
+    						{
+    							name: ":outbox_tray: Output",
+    							value: "Nothing here :innocent:"
+    						}
+  	  				],
+  	  				footer: {
+  	  					text: "PhoenixEval"
+  	  				}
+  	  			}
+  	  		})
+  	  	} else {
+  	  		message.channel.send({
+  	  			embed: {
+    					fields: [
+    						{
+    							name: ":inbox_tray: Input",
+    							value: "```" + msg + "```"
+    						},
+    						{
+	    						name: ":outbox_tray: Output",
+  	  						value: "```" + result + "```"
+    						}
+    					],
+    					footer: {
+    						text: "PhoenixEval"
+    					}
+	    			}
+  	  		})
+    		}
+   		} catch (e) {
+   			message.channel.send({
+  	  			embed: {
+  	  				description: ":dizzy_face: Something went wrong",
+    					fields: [
+    						{
+    							name: ":inbox_tray: Input",
+    							value: "```" + msg + "```"
+    						},
+    						{
+    							name: "Error content",
+    							value: "```" + e + "```"
+    						}
+    					],
+    					footer: {
+    						text: "PhoenixEval"
+    					}
+	    			}
+  	  		})
+   		}
+  	}
+	}
+})
+  }
+ else errors.owneronly(message.channel);
 });
 
 
