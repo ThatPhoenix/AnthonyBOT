cb3785bd38d9c268e130c12731922f9bc4c30130
Phoenix <theemaraldblock@gmail.com>
2018-08-17 19:59:20 +0100

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -3,11 +3,12 @@ const Discord = require('discord.js');
 exports.run = async(Anthony, message, args, tools) => {
 
   const embed = new Discord.MessageEmbed()
-    .setColor(client.color)
+    .setColor(Anthony.color)
     .setTitle(`${message.guild.name} - Information`)
       .addField("Aww Your Inviting Me! Thank You! :D"); 
     .setDescription('**Invite URL: \nhttps://discordapp.com/api/oauth2/authorize?client_id=465539661582958592&permissions=8&scope=bot**')
     .setFooter(Anthony.footer);
   
   message.channel.send(embed);
+};
   
