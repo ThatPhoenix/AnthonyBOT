64515cc463aa548c123cbd5c9cd30be0125b1829
Phoenix <theemaraldblock@gmail.com>
2018-08-17 19:47:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -20,9 +20,6 @@ Anthony.on("message", async message => {
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
  
-if(command === "invite") {
-  message.channel.send("https://discordapp.com/api/oauth2/authorize?client_id=465539661582958592&permissions=8&scope=bot")
- }
   if(command === "ping") {
     const PingMSG = await message.channel.send("Pinging Around The World :D");
     PingMSG.edit(`Pong! Anthony's Ping is ${PingMSG.createdTimestamp - message.createdTimestamp}ms. API's ping is ${Math.round(Anthony.ping)}ms`);
