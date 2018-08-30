c8d5324b538dee599fb89fbd048887db789bcb1b
Phoenix <theemaraldblock@gmail.com>
2018-08-30 12:23:54 +0100

    Create stats.js

---
+++ commands/stats.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+
+
+exports.run = function(Anthony, message, args) {
+  var embed = new Discord.RichEmbed();
+  embed.setTitle('Meh Stats <3')
+  embed.setColor('008000');
+  embed.addField('Uptime in mili-seconds', `${Anthony.uptime}`);
+  embed.addField('Users :O', `${Anthony.users.size}`)
+  embed.addField('Channels:', `${Anthony.channels.size}`)
+  embed.addField('Commands Are in BETA/Alpha')
+  embed.addField(':copyright: Phoenix Systems 2017-2020. All rights reserved.', 'This is a **Phoenix Systems Corporation Bot** And copyrighted and owned by Phoenix#8196 :copyright: **Phoenix Systems Corporation**')
+  embed.setFooter('Anthony's Stats')
+  embed.setTimestamp()
+
+  message.channel.sendEmbed(embed);
+};
+
+
+
+exports.help = {
+name: 'stats',
+};
