c7d19c1c03dfc89584b37c914fc8561278646a4d
Phoenix <theemaraldblock@gmail.com>
2018-09-03 11:20:38 +0100

    Update yomomma.js

--- commands/yomomma.js
+++ commands/yomomma.js
@@ -1,7 +1,7 @@
 const AnthonyHTTP = require("snekfetch");
 
 exports.run async (Anthony, AnthonyMSG) => {
-const AnthonyID await AnthonyHTTP.get("http://api.yomomma.info").then(data => JSON.parse(data.text));
+const AnthonyID await AnthonyHTTP.get('http://api.yomomma.info').then(data => JSON.parse(data.text));
 return AnthonyMSG.channel.send(':rofl: *${AnthonyID.joke}* \n :rofl:')
 };
 
