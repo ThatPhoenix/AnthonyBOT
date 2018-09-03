02df7d439541432106a099f8a920ec3db333fec5
Phoenix <theemaraldblock@gmail.com>
2018-09-03 16:51:35 +0100

    Update and rename yomomma.js to insultme.js

---
+++ commands/insultme.js
@@ -0,0 +1,17 @@
+const AnthonyInsult = require("../insult.json")
+
+exports.run async (Anthony, message) => {
+message.channel.send(":warning: | This can really Hurt User's feelings Some jokes may contain Swears So please proceed with caution and do not DM the owner saying Anthony insulted you :warning:
+message.channel.send("Do You Still Proceed? N For No Y for Yes")
+  if (message.content.startsWith("Y")) {
+ message.channel.send("Going on the dark edge aye?")
+    message.channel.send(AnthonyInsult[Math.floor(Math.random() * 4) +1]);
+  }
+  if (message.content.startsWith("N")) {
+    message.channel.send("Cancelled Prompt Good Choice :D stay on the safe side")
+  }
+}
+exports.help = {
+name: "insultme"
+};
+
