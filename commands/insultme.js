6780aa43825641f5317e20de57920533876aeb86
Phoenix <theemaraldblock@gmail.com>
2018-09-04 17:01:45 +0100

    Update insultme.js

--- commands/insultme.js
+++ commands/insultme.js
@@ -1,11 +1,6 @@
 const AnthonyInsults = require('../insults.json');
-exports.run = function(Anthony, message, args){
-  message.channel.sendMessage(':warning: | Do you really want to continue becouse this can hurt your feelings Y for yes N for no')
-  if (message.content.startsWith("N") {
-      message.channel.send("Cancelled Prompt")
-}
-if (message.content.startsWith("Y") {
-    message.channel.send("Here is Your Insult..")
+exports.run = function(Anthony, message, args) {
+  message.channel.sendMessage(":warning: | This Hurts People's Feelings")
   message.channel.send(AnthonyInsults[Math.floor(Math.random() * 20) +1]);
 };
 
