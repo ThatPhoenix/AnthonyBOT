eee849a88691a9bfd825739426b92a49b5ee0b82
Phoenix <theemaraldblock@gmail.com>
2018-09-04 16:59:08 +0100

    Update insultme.js

--- commands/insultme.js
+++ commands/insultme.js
@@ -1,17 +1,14 @@
-const AnthonyInsult = require("../insults.json")
-const Discord = require("discord.js")
-exports.run async (Anthony, message) => {
-message.channel.send(":warning: | This can really Hurt User's feelings Some jokes may contain Swears So please proceed with caution and do not DM the owner saying Anthony insulted you :warning:")
-message.channel.send("Do You Still Proceed? N For No Y for Yes")
-  if (message.content.startsWith("Y")) {
- message.channel.send("Going on the dark edge aye?")
-    message.channel.send(AnthonyInsult[Math.floor(Math.random() * 4) +1]);
-  }
-  if (message.content.startsWith("N")) {
-    message.channel.send("Cancelled Prompt Good Choice :D stay on the safe side")
-  }
+const AnthonyInsults = require('../insults.json');
+exports.run = function(Anthony, message, args){
+  message.channel.sendMessage(':warning: | Do you really want to continue becouse this can hurt your feelings Y for yes N for no')
+  if (message.content.startsWith("N") {
+      message.channel.send("Cancelled Prompt")
 }
-exports.help = {
-name: "insultme"
+if (message.content.startsWith("Y") {
+    message.channel.send("Here is Your Insult..")
+  message.channel.send(AnthonyInsults[Math.floor(Math.random() * 20) +1]);
 };
 
+exports.help = {
+  name: 'insultme'
+};
