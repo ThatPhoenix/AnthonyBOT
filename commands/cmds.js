5ddd483091ff85c9554e7718062236e1553d8e9d
Phoenix <theemaraldblock@gmail.com>
2018-09-04 18:02:22 +0100

    Update cmds.js

--- commands/cmds.js
+++ commands/cmds.js
@@ -5,7 +5,7 @@ exports.run = function(Anthony, message, args) {
   embed.setTitle('Anthony Cmds')
   embed.setDescription(':rofl: | **Fun Commands **: `8ball`, `shame`, `insultme \n:information_source: | **Info commands:** `credits`, `stats`,`ping`,`invite`');
   embed.setColor('#FF0000');
-  embed.addField('No Patreons yet.")
+  embed.addField('No Patreons yet.')
   embed.setFooter('We hope you have fun with Anthony')
 
   message.channel.sendEmbed(embed);
