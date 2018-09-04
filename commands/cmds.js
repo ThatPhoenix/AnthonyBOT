3e7c7dfabf49e2545088786ebd601e07d2f7efa2
Phoenix <theemaraldblock@gmail.com>
2018-09-04 18:05:47 +0100

    Update cmds.js

--- commands/cmds.js
+++ commands/cmds.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 exports.run = function(Anthony, message, args) {
   var embed = new Discord.RichEmbed();
   embed.setTitle('Anthony Cmds')
-  embed.setDescription(':rofl: | **Fun Commands **: `8ball`, `shame`, `insultme \n:information_source: | **Info commands:** `credits`, `stats`,`ping`,`invite`');
+  embed.setDescription(':rofl: | **Fun Commands **: `8ball`, `shame`, `insultme` \n:information_source: | **Info commands:** `credits`, `stats`,`ping`,`invite`');
   embed.setColor('#FF0000');
   embed.addField('No Patreons yet.')
   embed.setFooter('We hope you have fun with Anthony')
