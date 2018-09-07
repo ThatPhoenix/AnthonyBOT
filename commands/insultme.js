142d58d29a5506d05c7a2c8e876b3d2cea8ea0f6
Phoenix <theemaraldblock@gmail.com>
2018-09-07 19:34:55 +0100

    Update insultme.js

--- commands/insultme.js
+++ commands/insultme.js
@@ -1,6 +1,6 @@
 const AnthonyInsults = require('../insults.json');
 exports.run = function(Anthony, message, args) {
-  message.channel.sendMessage(":warning: | This Hurts People's Feelings")
+  message.channel.sendMessage(":warning: | This can offend you and also hurt your feelings be warned ")
   message.channel.send(AnthonyInsults[Math.floor(Math.random() * 4) +1]);
 };
 
