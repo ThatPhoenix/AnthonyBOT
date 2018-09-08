2831e2ab8de047c34d379401eb2a7b4545a545c2
Phoenix <theemaraldblock@gmail.com>
2018-09-08 11:23:27 +0100

    Update insultme.js

--- commands/insultme.js
+++ commands/insultme.js
@@ -1,12 +1,7 @@
 const AnthonyInsults = require('../insults.json');
 exports.run = function(Anthony, message, args) {
-  message.channel.sendMessage(":warning: | This can offend you and also hurt your feelings be warned Do you still proceed? N/Y?")
-  if (message.content.startsWith("Y") {
+  message.channel.sendMessage(":warning: | This can offend you and also hurt your feelings")
       message.channel.send(AnthonyInsults[Math.floor(Math.random() * 4) +1]);
-}
-
-if (message.content.startsWith("N") {
-      message.channel.send("Prompt Cancelled")
 }
   
 };
