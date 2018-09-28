d754a08156a743b50e80996071b0cc5622767246
Phoenix <theemaraldblock@gmail.com>
2018-09-28 22:19:41 +0100

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -4,5 +4,6 @@ exports.run = async(Anthony, message, args, tools) => {
 
       message.channel.send("Aww Your Inviting Me! Thank You! :D"); 
     message.channel.send("**Invite URL: \nhttps://discordapp.com/api/oauth2/authorize?client_id=465539661582958592&permissions=8&scope=bot**")
+    message.channels.send("**Support Server Invite: \nhttps://discord.gg/fYhSCSh**")
 };
   
